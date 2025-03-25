cb0a78b81706ed7cec52f60e5d412662264f2a56
Artem <webgid.main@gmail.com>
2025-03-25 16:45:33 +0200

    change title

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ app.post('/process-form', async (req, res) => {
         const domainList = domains.split('\n').map(domain => domain.trim());
         console.log({ email, apiKey, domains, ipAddresses, settings });
         const logs = await cloudflare.setupCloudflare({ email, apiKey, domains: domainList, ipAddresses, settings });
-        //console.log(logs)
+        console.log(logs)
         const nowInKyiv = DateTime.now().setZone("Europe/Kyiv").toFormat("dd.MM.yyyy HH:mm")
+
         //await spreadsheet.addRows(process.env.SHEET,process.env.TAB,logs.map(i=>[nowInKyiv,...i]))
         res.render('result', { logs});
     } catch (error) {
