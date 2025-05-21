f91cab2014ed1c35e0e79f908fa50d927a8d650e
Artem <webgid.main@gmail.com>
2025-05-21 18:36:00 +0300

    new ns servers

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ try {
     const bodyParser = require('body-parser');
     const {processForm} = require('./main')
     const app = express();
-    const port = 5217;
+    const port = 5218;
     app.set('view engine', 'ejs');
     app.use(express.static('public'));
     app.use(bodyParser.urlencoded({extended: true}));
